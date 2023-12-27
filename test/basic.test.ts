504808962a4502e528e685a2c220c1b229ae0f52
Rodrigo P <rspimentel@worten.pt>
2023-12-26 23:30:18 -0100

    fix: add dummy test

---
+++ test/basic.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { fileURLToPath } from 'node:url'
+import { setup, $fetch } from '@nuxt/test-utils'
+
+describe('ssr', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+  })
+
+  it('renders the index page', async () => {
+    // Make a better test
+    expect(true).toBe(true)
+  })
+})
